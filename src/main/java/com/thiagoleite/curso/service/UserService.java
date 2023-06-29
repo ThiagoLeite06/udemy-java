79e549fa4796b6a6da2431ef7ac2da42195bed51
Thiago Leite <thiago.0686@gmail.com>
2023-06-28 23:41:25 -0300

    Fix application db connection

---
+++ src/main/java/com/thiagoleite/curso/service/UserService.java
@@ -0,0 +1,22 @@
+package com.thiagoleite.curso.service;
+
+import com.thiagoleite.curso.entities.User;
+import com.thiagoleite.curso.repository.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+}
